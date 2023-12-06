a6cb62cd109d85f9d754bd08a3ba897770ccbb0d
정재웅 <jweidon0821@gmail.com>
2023-12-06 13:15:08 +0900

    feat: 레시피 메서드 스켈레톤 코드 작성

--- src/main/java/cookingforeveryone/controller/IngredientController.java
+++ src/main/java/cookingforeveryone/controller/IngredientController.java
@@ -10,25 +10,25 @@ public class IngredientController {
     // 추가
     @PostMapping("/api/add")
     public String add(@RequestBody Ingredient ingredient) {
-        // 여기에서 받은 ingredient를 데이터베이스에 저장하는 등의 비즈니스 로직을 수행해야 합니다.
-        // 예제에서는 간단히 추가된 재료의 이름을 반환합니다.
+        // 입력 받은 ingredient를 데이터베이스에 저장하는 등의 비즈니스 로직을 수행합니다.
+        // 입력된 식재료 이름을 반환합니다.
         return "Added ingredient: " + ingredient.getName();
     }
 
     // 삭제
     @DeleteMapping("api//delete/{id}")
     public String delete(@PathVariable Long id) {
-        // 여기에서 주어진 id에 해당하는 재료를 데이터베이스에서 삭제하는 등의 비즈니스 로직을 수행해야 합니다.
-        // 예제에서는 간단히 삭제된 재료의 ID를 반환합니다.
+        // 주어진 id에 해당하는 재료를 데이터베이스에서 삭제하는 등의 비즈니스 로직을 수행합니다.
+        // 삭제된 재료의 ID를 반환합니다.
         return "Deleted ingredient with ID: " + id;
     }
 
     // 수정
     @PutMapping("api/update/{id}")
     public String update(@PathVariable Long id, @RequestBody Ingredient updatedIngredient) {
-        // 여기에서 주어진 id에 해당하는 재료를 데이터베이스에서 찾아 업데이트하는 등의 비즈니스 로직을 수행해야 합니다.
-        // updatedIngredient에는 업데이트된 정보가 담겨 있습니다.
-        // 예제에서는 간단히 업데이트된 재료의 ID와 이름을 반환합니다.
+        // 주어진 id에 해당하는 재료를 데이터베이스에서 찾아 업데이트하는 등의 비즈니스 로직을 수행합니다.
+        // updatedIngredient에는 업데이트된 식재료의 정보가 담겨 있습니다.
+        // 업데이트된 재료의 ID와 이름을 반환합니다.
         return "Updated ingredient with ID " + id + ": " + updatedIngredient.getName();
     }
 }
--- src/main/java/cookingforeveryone/controller/RecipeController.java
+++ src/main/java/cookingforeveryone/controller/RecipeController.java
@@ -1,4 +1,41 @@
 package cookingforeveryone.controller;
 
+import cookingforeveryone.domain.Recipe;
+import cookingforeveryone.dto.RecipeDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/recipes")
 public class RecipeController {
+
+    // 맞춤형 레시피 추천
+    @GetMapping("/recipes")
+    public List<RecipeDto> getRecipes() {
+        // 맞춤형 레시피 추천 로직을 수행합니다.
+        // 입력된 식재료를 기반으로 데이터베이스에서 레시피를 조회하는 등의 작업을 수행합니다.
+        // 디폴트로 빈 리스트를 반환합니다.레시피를 찾으면 RecipeDto를 생성하여 반환합니다.
+        return List.of();
+    }
+
+    // 레시피 상세 정보 조회
+    @GetMapping("/recipe/detail/{recipeId}")
+    public RecipeDto getRecipeDetail(@PathVariable Long recipeId) {
+        // 주어진 recipeId에 해당하는 레시피의 상세 정보 조회 로직을 수행합니다.
+        // 데이터베이스에서 레시피를 조회하고 recipeId에 해당하는 레시피 정보를 찾습니다.
+        // 디폴트로 빈 RecipeDto를 반환합니다. 레시피를 찾으면 해당 RecipeDto를 생성하여 반환합니다.
+        return new RecipeDto();
+    }
+
+    // 랜덤 메뉴 추천
+    @GetMapping("/recipe/random")
+    public RecipeDto getRandomRecipe() {
+        // 데이터베이스에서 랜덤하게 하나의 레시피를 조회하여 반환하는 로직을 수행합니다.
+        // 디폴트로 빈 RecipeDto를 반환합니다. 랜덤한 레시피를 찾으면 해당 RecipeDto를 생성하여 반환합니다.
+        return new RecipeDto();
+    }
 }
